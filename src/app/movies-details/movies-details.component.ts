259dd3bc8049ebaa86cf30ee4f457bfb4243706a
domuserk <rgdesenhos321@gmail.com>
2021-07-29 20:52:25 -0300

    add movies details search by input

--- src/app/movies-details/movies-details.component.ts
+++ src/app/movies-details/movies-details.component.ts
@@ -1,6 +1,7 @@
 import { MoviesService } from './../movies/services/movies.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-movies-details',
@@ -21,10 +22,18 @@ export class MoviesDetailsComponent implements OnInit {
   searchMovies:any;
   button:boolean = false;
   showAllCasts: string = 'Show All';
+  messageToShow: any;
+  messageToShowEquals:any;
+  movies: any;
+  routeActive:boolean = false;
+  moviesToShow: boolean = false;
+  loadingSecondCard: boolean = false;
+  movieName:any;
 
   constructor(
     private route: ActivatedRoute,
     private moviesService: MoviesService,
+    private formBuilder: FormBuilder,
     private router: Router
     ) { }
 
@@ -42,8 +51,15 @@ export class MoviesDetailsComponent implements OnInit {
    }else {
      this.params = true;
    }
+
+   this.movieName = this.formBuilder.group ({
+    title: ['']
+  })
+
+    this.getSearchBar()
+    this.verifyEqualsSearch()
+    this.getAllMovies(this.id)  
     this.getCredits(this.id)
-    this.getAllMovies(this.id)   
     this.getimagesMovie(this.id)
   }
 
@@ -51,14 +67,11 @@ export class MoviesDetailsComponent implements OnInit {
     this.imagesMovie = await this.moviesService.imagesMovies(id)
   }
   
-  async getAllMovies(id) {
-    this.allMovies = await this.moviesService.getAllMovies(this.id)
-  }
-
+  
   async getCredits(id) {
     this.credits = await this.moviesService.getCredits(this.id)
   }
-
+  
   toggleCasts() {
     if (this.button == true) {
       this.button = false;
@@ -66,6 +79,49 @@ export class MoviesDetailsComponent implements OnInit {
     }else {
       this.button = true;
       this.showAllCasts = 'Toggle'
+    }
+  }
+  
+  onClick() {
+    this.routeActive = true
+  }
+  
+  async getMovieName(movieName?) {
+    if (movieName != undefined && movieName != '' ) {
+      this.moviesToShow = true;
+      this.movies = await this.moviesService.getMovies(movieName)
+    }
+    return null;
+  }
+  
+  async getSearchBar() {
+    if (this.messageToShow != undefined || this.messageToShow != '') {
+      await this.moviesService.movieName.subscribe(msg => {
+        this.messageToShow = msg;
+        if (this.messageToShow != this.messageToShowEquals) {
+          this.getMovieName(this.messageToShow)
+        }
+      })
+    } else {
+      return null;
+    }
+  }
+  
+  async verifyEqualsSearch() {
+    await this.moviesService.movieName.subscribe(msg => {
+      this.messageToShowEquals = msg;
+    })
+    if (this.messageToShowEquals === this.messageToShow) return null;
+  }
+
+  async getAllMovies(id) {
+    if (this.messageToShow == '' || this.messageToShow == null){
+      try {
+        this.allMovies = await this.moviesService.getAllMovies(this.id)
+      } catch (err) {
+        console.log(err)
+      }
+      return null;
     }
   }
 }
